2d9818c847fbf78bc3c44e54c3aa0ed8713f27cd
aflaag <alessio.bandiera02@gmail.com>
2024-03-12 11:15:34 +0100

    script updated

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 NOTES_PATHS=`find -maxdepth 1 -type d`
-TO_AVOID="^(|\.|\./\.git)$"
+NOTES_PATHS="$NOTES_PATHS ./packages/Nyx"
+
+TO_AVOID="^(|\.|\./\.git|\./packages)$"
 
 for PATH in $NOTES_PATHS; do
     if ! [[ "$PATH" =~ $TO_AVOID ]]
     then
         cd $PATH
+        echo "##### INSIDE $PATH ##### "
         /usr/bin/git checkout main
         cd ..
     fi
