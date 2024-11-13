fa677ac9bc5ae73dc249ad6a3178e014cbdfe309
aflaag <alessio.bandiera02@gmail.com>
2024-11-13 23:34:45 +0100

    changed shell.nix formatting

--- shell.nix
+++ shell.nix
@@ -1,6 +1,64 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [
-      (texlive.combine {inherit (texlive) scheme-basic latexmk xcolor eurosym titlesec blindtext listings footmisc mdframed etoolbox zref needspace pgf parskip enumitem csquotes float bbm bbm-macros tcolorbox environ varwidth biblatex esint nicematrix stmaryrd mathtools extarrows jknapltx pgfplots multirow ifoddpage relsize karnaugh-map xstring circuitikz ulem tikzfill pdfcol cleveref collection-fontsrecommended fifo-stack tabto-ltx totcount tikzmark pdftex esint-type1 alg algorithms algorithm2e algpseudocodex algorithmicx algobox algxpar todonotes;})
+  nativeBuildInputs = with pkgs.buildPackages; [
+    (texlive.combine {
+      inherit (texlive)
+        scheme-basic
+        latexmk
+        xcolor
+        eurosym
+        titlesec
+        blindtext
+        listings
+        footmisc
+        mdframed
+        etoolbox
+        zref
+        needspace
+        pgf
+        parskip
+        enumitem
+        csquotes
+        float
+        bbm
+        bbm-macros
+        tcolorbox
+        environ
+        varwidth
+        biblatex
+        esint
+        nicematrix
+        stmaryrd
+        mathtools
+        extarrows
+        jknapltx
+        pgfplots
+        multirow
+        ifoddpage
+        relsize
+        karnaugh-map
+        xstring
+        circuitikz
+        ulem
+        tikzfill
+        pdfcol
+        cleveref
+        collection-fontsrecommended
+        fifo-stack
+        tabto-ltx
+        totcount
+        tikzmark
+        pdftex
+        esint-type1
+        alg
+        algorithms
+        algorithm2e
+        algpseudocodex
+        algorithmicx
+        algobox
+        algxpar
+        todonotes
+      ;}
+    )
   ];
 }
