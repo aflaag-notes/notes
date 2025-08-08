6f208b6d850b9728424772f34a670035b9e57198
aflaag <alessio.bandiera02@gmail.com>
2025-08-08 14:13:09 +0200

    fixed checkout

--- checkout.sh
+++ checkout.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+GIT=`which git`
+
 NOTES_PATHS=`find -maxdepth 1 -type d`
 NOTES_PATHS="$NOTES_PATHS ./packages/Nyx"
 
@@ -9,8 +11,8 @@ for PATH in $NOTES_PATHS; do
     if ! [[ "$PATH" =~ $TO_AVOID ]]
     then
         cd $PATH
-        echo "##### INSIDE $PATH ##### "
-        /etc/profiles/per-user/aless/bin/git checkout main
+        echo "##### INSIDE $PATH #####"
+        $GIT checkout main
         cd ..
     fi
 done
